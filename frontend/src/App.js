b3e585166df626be6166cc2d357ebeb0b2ab4f23
danielyoo98 <dyoo9@my.bcit.ca>
2020-02-28 20:24:05 -0800

    restructured the project (i.e. created components directory)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RangeSlider from './components/RangeSlider';
 
 function App() {
   return (
     <div className='App'>
-      
+      <RangeSlider />
     </div>
   );
 }
