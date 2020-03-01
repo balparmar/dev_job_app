fad7e6bf89857ccec6aee12919c1b6b88b60b6a9
balparmar <bparmar14@my.bcit.ca>
2020-02-29 20:52:17 -0800

    added a header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import RangeSlider from './components/RangeSlider';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div className='bg-red-500 w-screen h-screen'>
+       <MainHeader/>
       <RangeSlider />
     </div>
   );
