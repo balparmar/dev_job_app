43b8d7afec48d588a872b5aa3aa0d9c08c1207fa
Ken <kenichikona@gmail.com>
2020-03-01 15:32:26 -0800

    finished main layout resposiveness

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,20 @@ import MainHeader from './components/MainHeader';
 
 function App() {
   return (
-    <div className='bg-red-500 w-screen h-screen'>
-       <MainHeader/>
-      <RangeSlider />
+    <div>
+      <div className='h-full'>
+        <MainHeader className='absolute top-0 sticky z-50' />
+        <div className='flex h-full justify-start items-start flex-wrap'>
+          <div className='w-full bg-red-500 h-full md:w-1/3'>Column 1</div>
+          <div className='w-full bg-blue-500 h-full md:w-1/3'>Column 2</div>
+          <div className='w-full bg-gray-500 h-full md:w-1/3'>
+            <div className='bg-pink-300 h-2/3 w-full'>Side Column 1</div>
+            <div className='bg-pink-300 h-1/3 w-full'>Side Column 2</div>
+          </div>
+        </div>
+      </div>
+
+      {/* <RangeSlider /> */}
     </div>
   );
 }
